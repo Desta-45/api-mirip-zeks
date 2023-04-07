fa85e9a82a126e53111d0dc16d4fba1aa379032e
Desta-45 <70932926+Desta-45@users.noreply.github.com>
2023-04-07 10:06:47 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc' 
-global.apikey = ["Zeeoneofc", "Alphabot", "Alpha"]
+global.apikey = ["Zeeoneofc", "Alphabot", "dsxbot"]
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
